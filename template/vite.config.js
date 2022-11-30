6b1b1344d019f4b4e0983ecbb3c11d22b170fb5b
Taly <vitalik7tv@yandex.ru>
2022-11-30 15:11:59 +0300

    update vite config

--- template/vite.config.js
+++ template/vite.config.js
@@ -1,26 +1,21 @@
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import path from 'path';
 
-export default ({ command }) => {
-  if (command === 'build') return {
-    build: {
-      rollupOptions: {
-        input: {
-          VAR_TOOL_NAME: path.resolve(__dirname, 'src/index.ts'),
-        },
-
-        output: {
-          name: 'VAR_TOOL_NAME',
-          format: 'umd',
-          dir: 'dist',
-          entryFileNames: '[name].js',
-        },
-      },
+export default {
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      name: 'VAR_TOOL_NAME',
+      formats: ['umd'],
+      fileName: () => 'VAR_TOOL_NAME.js'
     },
-  };
+  },
 
-  return {
-    server: {
-      port: '3300'
-    }
-  };
+  server: {
+    port: '3300'
+  },
+
+  plugins: [
+    cssInjectedByJsPlugin(),
+  ]
 }
